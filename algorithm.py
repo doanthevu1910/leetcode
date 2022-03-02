54cc8d05c6e0293d1373423b02eb6ebbcd5af181
doanthevu1910 <doanthevu1910@gmail.com>
2022-03-02 11:54:19 +0100

    Updated algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -18,3 +18,92 @@ def searchInsert(nums, target):
             return i
     return i
 
+num = [1, 2, 3]
+
+def sortedSquares(nums):
+    result = []
+    for i in range(len(nums)):
+        result.append(nums[i]**2)
+    return sorted(result)
+
+sortedSquares([-4,-1,0,3,10])
+
+num = [-4,-1,0,3,10]
+
+# a = num[-3:]
+#
+# num = a + num[0:2]
+
+def rotate(nums, k):
+    for i in range(k):
+        nums.insert(0, nums[-1])
+        nums.pop()
+
+prices = [7,1,5,3,6,4]
+
+def maxProfit(prices):
+    test = []
+
+    for i in range(len(prices)):
+        last = prices[-i]
+        for j in range(i):
+            test.append(last - prices[j])
+
+    return max(test)
+
+maxProfit(prices)
+
+
+def moveZeroes(nums):
+    for i in range(len(nums)):
+        x = nums[i]
+        if x == 0:
+            nums.insert(len(nums), x)
+            nums.pop(i)
+
+nums = [0,0,1]
+moveZeroes(nums)
+nums
+
+def reverseString(s):
+    for i in range(len(s)):
+        s.insert(0, s[-1])
+        s.pop()
+
+s = ["h","e","l","l","o"]
+a = reverseString(s)
+
+s = "Let's take LeetCode contest"
+
+def reverseWords(s):
+    split = s.split(' ')
+    words = []
+    for i in range(len(split)):
+        words.append(split[i][::-1])
+
+    return ' '.join(words)
+
+reverseWords(s)
+
+mat = [[1,2],[3,4]]
+
+import numpy as np
+
+def matrixReshape(mat, r, c):
+    if len(mat)*len(mat[0]) != r*c:
+        return mat
+    else:
+        return np.reshape(mat, (r, c))
+
+matrixReshape(mat, 1, 4)
+
+def generate(numRows):
+    pt = []
+    for row_num in range(numRows):
+        row = [1] * (row_num + 1)
+        for index in range(1, len(row) - 1):
+            row[index] = pt[row_num - 1][index - 1] + pt[row_num - 1][index]
+        pt.append(row)
+    return pt
+
+generate(5)
