1224cd1dac0366eaabddab975f07e07d833fe44e
doanthevu1910 <doanthevu1910@gmail.com>
2022-03-04 22:00:37 +0100

    Updated top-100.py

--- top-100.py
+++ top-100.py
@@ -5,3 +5,25 @@ from collections import Counter
 if Counter(nums)[nums[0]] > len(nums)/2:
     print(nums[0])
 
+def isPalindrome(s):
+    s = ''.join(ch for ch in s if ch.isalnum()).lower()
+    return s == s[::-1]
+
+s = 'babad'
+
+sub = [s[i: j] for i in range(len(s)) for j in range(i + 1, len(s) + 1)]
+
+isPalindrome(sub[0])
+
+def longestPalindrome(s):
+    sub = [s[i: j] for i in range(len(s)) for j in range(i + 1, len(s) + 1)]
+
+    palin = []
+
+    for i in range(len(sub)):
+        if isPalindrome(sub[i]) == True:
+            palin.append(sub[i])
+
+    return max(palin, key=len)
+
+longestPalindrome(s)
