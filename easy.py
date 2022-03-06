cde42cd7795fe8b4cfd940d6f715f1041a0ae964
doanthevu1910 <doanthevu1910@gmail.com>
2022-03-06 16:32:53 +0100

    Updated easy.py

--- easy.py
+++ easy.py
@@ -25,3 +25,24 @@ int('12', 2)
 #             result += 1
 #         result += i - 1
 # return result
+
+s = "Hello, my name is John"
+
+len(s.split())
+
+s = "5F3Z-2e-9-w"
+
+s = s.replace('-', '').upper()
+
+nums = [2,5,1,3,4,7]
+
+first = nums[:len(nums)//2]
+second = nums[len(nums)//2:]
+
+result = []
+
+for i in range(len(first)):
+    result.append(first[i])
+    result.append(second[i])
+
+result
