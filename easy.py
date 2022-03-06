c3f15459d426887a4dc2ddae8cefb74c8536d54b
doanthevu1910 <doanthevu1910@gmail.com>
2022-03-06 09:53:20 +0100

    Updated easy.py

--- easy.py
+++ easy.py
@@ -16,3 +16,12 @@ bin(1010+1011)
 
 int('12', 2)
 
+# result, freq = 0, sorted([s.count(i) for i in set(s)])
+# for i in freq:
+#     if i % 2 == 0:
+#         result += i
+#     else:
+#         if result % 2 == 0:
+#             result += 1
+#         result += i - 1
+# return result
