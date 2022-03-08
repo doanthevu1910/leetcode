0c6dae9b6752295074c10890b2054753c94ad8d5
doanthevu1910 <doanthevu1910@gmail.com>
2022-03-08 15:32:10 +0100

    Updated easy.py

--- easy.py
+++ easy.py
@@ -54,3 +54,18 @@ stones = "aAAbbbb"
 
 Counter(jewels)
 sum += Counter(stones)[jewels[i]]
+
+class Solution(object):
+    def restoreString(self, s, indices):
+        """
+        :type s: str
+        :type indices: List[int]
+        :rtype: str
+        """
+        answer = ''
+
+        for i in range(len(indices)):
+            answer += s[indices.index(i)]
+
+        return answer
+
