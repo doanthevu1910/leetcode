d1bbcc031a882ac67dc5d76dc8fcb77686c65a4f
doanthevu1910 <doanthevu1910@gmail.com>
2022-03-10 12:20:11 +0100

    Updated easy.py

--- easy.py
+++ easy.py
@@ -76,3 +76,21 @@ def function(head):
             head = head.next
         return num
 
+class Solution(object):
+    def numberOfSteps(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+
+        steps = 0
+
+        while num > 0:
+            if num % 2 == 0:
+                num //= 2
+                steps += 1
+            else:
+                num -= 1
+                steps += 1
+
+        return steps
