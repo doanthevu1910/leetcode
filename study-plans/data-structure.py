3f6a2f0974c9e932fe9340090dcb4243bf4c1e83
doanthevu1910 <doanthevu1910@gmail.com>
2022-03-13 11:23:29 +0100

    Updated easy.py

--- study-plans/data-structure.py
+++ study-plans/data-structure.py
@@ -154,5 +154,25 @@ def combinations(nums):
 
         return result
 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if root and root.val == val:
+            return root
 
+        else:
+            if root:
+                return self.searchBST(root.left, val) or self.searchBST(root.right, val)
+
+        return None
 
