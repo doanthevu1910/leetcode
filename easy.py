c4184fc90c460986bf431d7c859420fa2b63899f
doanthevu1910 <doanthevu1910@gmail.com>
2022-03-13 12:02:16 +0100

    Updated easy.py

--- easy.py
+++ easy.py
@@ -223,3 +223,17 @@ class Solution(object):
         return int(''.join(ans))
 
 
+class Solution(object):
+    def minOperations(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i - 1]:
+                count += nums[i - 1] - nums[i] + 1
+                nums[i] = nums[i - 1] + 1
+
+        return count
