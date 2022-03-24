67b73a215eee9e5b125700e62551d5922a6b04ea
doanthevu1910 <doanthevu1910@gmail.com>
2022-03-24 09:25:07 +0100

    Updated programming.py

--- study-plans/programming.py
+++ study-plans/programming.py
@@ -303,3 +303,6 @@ class Solution(object):
 
         return [check(nums[i:j + 1]) for i, j in zip(l, r)]
 
+def canMeasureWater(self, x, y, z):
+    from fractions import gcd
+    return z == 0 or x + y >= z and z % gcd(x, y) == 0
