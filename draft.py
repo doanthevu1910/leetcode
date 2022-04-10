57812cddbcf54e47d495e75de64d65612ed854e7
doanthevu1910 <doanthevu1910@gmail.com>
2022-04-10 15:01:28 +0200

    Updated programming.py

--- draft.py
+++ draft.py
@@ -8,3 +8,30 @@ def CallOption(x):
     return 0
 
 CallOption(50)
+
+x = [[1, 0], [0, 1]]
+y = []
+
+for element in x:
+    y.append(element[::-1])
+
+y = []
+
+import numpy as np
+
+for element in x:
+    y.append(np.flip(element))
+
+x = True
+y = False
+
+True * y
+
+def function(x):
+    return 'ghi'*x
+
+function(True)
+
+x = [10, 20, 30]
+
+x.index(30)
