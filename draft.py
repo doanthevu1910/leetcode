29158549b7453666bc0fc3a845a6c8eeb6d4a3f0
doanthevu1910 <doanthevu1910@gmail.com>
2022-04-14 12:34:17 +0200

    Updated draft.py

--- draft.py
+++ draft.py
@@ -35,3 +35,42 @@ function(True)
 x = [10, 20, 30]
 
 x.index(30)
+
+lst = [0, 2, 'a', 'A', 'B', 'c', 8]
+
+a = type(lst[3])
+
+def count_character(lst):
+    countupper = 0
+    countlower = 0
+    countnumber = 0
+
+    for i in range(len(lst)):
+
+        if type(lst[i]) == int:
+            countnumber += 1
+
+        else:
+            if lst[i].isupper():
+                countupper += 1
+
+            elif lst[i].islower():
+                countlower += 1
+
+    return f'upper: {countupper}, lower: {countlower}, number: {countnumber}'
+
+count_character(lst)
+
+x = ['1', 'a', 'X', '2', 'a']
+
+from collections import Counter
+
+lower = [char for char in x if char.islower()]
+
+y = Counter(lower)
+
+x = [1, 2, 3]
+y = ['v', 't', 'd']
+
+dict(zip(x, y))
+
