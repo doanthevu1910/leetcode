f70afbb107f077db84c3158e53e7133df3dfffad
doanthevu1910 <doanthevu1910@gmail.com>
2022-04-20 23:11:37 +0200

    Updated draft.py

--- draft.py
+++ draft.py
@@ -74,3 +74,48 @@ y = ['v', 't', 'd']
 
 dict(zip(x, y))
 
+buyers = {'buyer1': [24, 16, 13, 6, 5], 'buyer2': [23, 21, 19, 10, 7], 'buyer3': [21, 20, 15, 13, 10], 'buyer4': [20, 17, 9, 6, 5]}
+
+sellers = {'seller1': [8, 10, 14, 19, 21], 'seller2': [6, 13, 16, 21, 23], 'seller3': [8, 12, 13, 22, 23], 'seller4': [8, 9, 13, 21, 24]}
+
+sum()
+
+x = buyers['buyer1']
+y = sellers['seller1']
+
+x
+y
+
+def CountSubtract(set1, set2):
+    count = 0
+    for i in range(len(set1)):
+        if set1[i] - set2[i] >= 0:
+            count += 1
+
+    return count
+
+ans = 0
+
+buyers = {'buyer1': [15, 14, 12, 5, 3], 'buyer2': [20, 13, 6, 3, 2], 'buyer3': [15, 13, 11, 7, 4], 'buyer4': [19, 17, 16, 11, 3]}
+sellers = {'seller1': [2, 6, 8, 11, 21], 'seller2': [6, 7, 12, 15, 17], 'seller3': [5, 6, 7, 11, 19], 'seller4': [3, 5, 7, 12, 13]}
+
+CountSubtract(buyers['buyer1'], sellers['seller1'])
+CountSubtract(buyers['buyer2'], sellers['seller2'])
+CountSubtract(buyers['buyer3'], sellers['seller3'])
+CountSubtract(buyers['buyer4'], sellers['seller4'])
+
+buyers = {'buyer1': [15, 14, 12, 5, 3], 'buyer2': [20, 13, 6, 3, 2], 'buyer3': [15, 13, 11, 7, 4], 'buyer4': [19, 17, 16, 11, 3]}
+sellers = {'seller1': [2, 6, 8, 11, 21], 'seller2': [6, 7, 12, 15, 17], 'seller3': [5, 6, 7, 11, 19], 'seller4': [3, 5, 7, 12, 13]}
+
+demand = sorted(buyers['buyer1'] + buyers['buyer2'] + buyers['buyer3'] + buyers['buyer4'], reverse=True)
+
+supply = sorted(sellers['seller1'] + sellers['seller2'] + sellers['seller3'] + sellers['seller4'])
+
+count = 0
+
+for i in range(len(demand)):
+    if demand[i] >= supply[i]:
+        count += 1
+
+count
+
